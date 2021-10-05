22771f4a48af4d092c36da46b0ce404a5d14ca9c
FlavioLucasFer <flaviolucasfer@gmail.com>
2021-10-05 17:05:45 -0300

    DER v1.2

---
+++ database/migrations/2021_10_05_194725_create_user_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_games', function (Blueprint $table) {
+            $table->id();
+						$table->time('time_spent')->nullable();
+            $table->timestamps();
+						$table->softDeletes();
+						$table->unsignedBigInteger('user_id');
+						$table->unsignedBigInteger('game_id');
+						$table->foreign('user_id')->references('id')->on('users');
+						$table->foreign('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_games');
+    }
+}
